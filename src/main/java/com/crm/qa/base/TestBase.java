9fe93587f056cffb762c58dac503e2652e2bde06
Avadoot <avadootyadav123@gmail.com>
2020-09-20 19:26:55 +0530

    Major changes

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,60 +18,52 @@ import com.crm.qa.util.TestUtil;
 import com.crm.qa.util.WebEventListener;
 
 public class TestBase {
-	
-	public static WebDriver driver;
-	public static Properties prop;
-	public  static EventFiringWebDriver e_driver;
-	public static WebEventListener eventListener;
-	
-	public TestBase(){
-		try {
-			prop = new Properties();
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/crm"
-					+ "/qa/config/config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public static void initialization(){
-		String browserName = prop.getProperty("browser");
-		
-		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
-			driver = new ChromeDriver(); 
-		}
-		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
-			driver = new FirefoxDriver(); 
-		}
-		
-		
-		e_driver = new EventFiringWebDriver(driver);
-		// Now create object of EventListerHandler to register it with EventFiringWebDriver
-		eventListener = new WebEventListener();
-		e_driver.register(eventListener);
-		driver = e_driver;
-		
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-		
-		driver.get(prop.getProperty("url"));
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+    public static WebDriver driver;
+    public static Properties prop;
+    public static EventFiringWebDriver e_driver;
+    public static WebEventListener eventListener;
+
+    public TestBase() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/com/crm"
+                    + "/qa/config/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void initialization() {
+        String browserName = prop.getProperty("browser");
+
+        if (browserName.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equals("FF")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+
+        e_driver = new EventFiringWebDriver(driver);
+        // Now create object of EventListerHandler to register it with EventFiringWebDriver
+        eventListener = new WebEventListener();
+        e_driver.register(eventListener);
+        driver = e_driver;
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+        driver.get(prop.getProperty("url"));
+
+    }
+
 
 }
--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -3,6 +3,7 @@ package com.crm.qa.testcases;
 import java.io.File;
 import java.io.IOException;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -10,81 +11,86 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class FreeCrmTest {
 
-	static WebDriver driver;
-	static JavascriptExecutor js;
+    static WebDriver driver;
+    static JavascriptExecutor js;
 
-	@BeforeMethod
-	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");
-		driver = new ChromeDriver();
-		js = (JavascriptExecutor) driver;
-		driver.get("https://www.freecrm.com/index.html");
-	}
+    @BeforeMethod
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        driver.get("https://freecrm.co.in/index.html");
+    }
 
-	@Test
-	public void freeCrmTitleTest() throws InterruptedException, IOException {
-		String title = driver.getTitle();
-		System.out.println("title is: " + title);
-		getRunTimeInfoMessage("info", title);
+    @Test
+    public void freeCrmTitleTest() throws InterruptedException, IOException {
+        String title = driver.getTitle();
+        System.out.println("title is: " + title);
+        getRunTimeInfoMessage("info", title);
 
-		if (title.equals("Free CRM software in the cloud powers sales and customer serviceQQQQ")) {
-			getRunTimeInfoMessage("info", "title is correct!! YAY!!!");
-			Assert.assertTrue(true);
-		} else {
-			getRunTimeInfoMessage("error", "title is not correct!! BUG BUG BUG!!!");
-			takeScreenshot("freecrmloginpage");
-			Assert.assertTrue(false);
-		}
+        if (title.equals("Free CRM software in the cloud powers sales and customer serviceQQQQ")) {
+            getRunTimeInfoMessage("info", "title is correct!! YAY!!!");
+            Assert.assertTrue(true);
+        } else {
+            getRunTimeInfoMessage("error", "title is not correct!! BUG BUG BUG!!!");
+            takeScreenshot("freecrmloginpage");
+            Assert.assertTrue(false);
+        }
 
-	}
+    }
 
-	public static void getRunTimeInfoMessage(String messageType, String message) throws InterruptedException {
-		// Check for jQuery on the page, add it if need be
-		js.executeScript("if (!window.jQuery) {"
-				+ "var jquery = document.createElement('script'); jquery.type = 'text/javascript';"
-				+ "jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js';"
-				+ "document.getElementsByTagName('head')[0].appendChild(jquery);" + "}");
-		Thread.sleep(5000);
+    public static void getRunTimeInfoMessage(String messageType, String message) throws InterruptedException {
+        // Check for jQuery on the page, add it if need be
+        js.executeScript("if (!window.jQuery) {"
+                + "var jquery = document.createElement('script'); jquery.type = 'text/javascript';"
+                + "jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js';"
+                + "document.getElementsByTagName('head')[0].appendChild(jquery);" + "}");
+        Thread.sleep(5000);
 
-		// Use jQuery to add jquery-growl to the page
-		js.executeScript("$.getScript('https://the-internet.herokuapp.com/js/vendor/jquery.growl.js')");
+        // Use jQuery to add jquery-growl to the page
+        js.executeScript("$.getScript('https://the-internet.herokuapp.com/js/vendor/jquery.growl.js')");
 
-		// js.executeScript("$.getScript('/Users/NaveenKhunteta/Documents/workspace/Test/src/testcases/jquery.growl.js')");
+        // js.executeScript("$.getScript('/Users/NaveenKhunteta/Documents/workspace/Test/src/testcases/jquery.growl.js')");
 
-		// Use jQuery to add jquery-growl styles to the page
-		js.executeScript("$('head').append('<link rel=\"stylesheet\" "
-				+ "href=\"https://the-internet.herokuapp.com/css/jquery.growl.css\" " + "type=\"text/css\" />');");
-		Thread.sleep(5000);
+        // Use jQuery to add jquery-growl styles to the page
+        js.executeScript("$('head').append('<link rel=\"stylesheet\" "
+                + "href=\"https://the-internet.herokuapp.com/css/jquery.growl.css\" " + "type=\"text/css\" />');");
+        Thread.sleep(5000);
 
-		// jquery-growl w/ no frills
-		js.executeScript("$.growl({ title: 'GET', message: '/' });");
-		
-		if(messageType.equals("error")){
-			js.executeScript("$.growl.error({ title: 'ERROR', message: '"+message+"' });");
-		}else if(messageType.equals("info")){
-			js.executeScript("$.growl.notice({ title: 'Notice', message: '"+message+"' });");
-		}else if(messageType.equals("warning")){
-			js.executeScript("$.growl.warning({ title: 'Warning!', message: '"+message+"' });");
-		}
+        // jquery-growl w/ no frills
+        js.executeScript("$.growl({ title: 'GET', message: '/' });");
 
-		// jquery-growl w/ colorized output
+        if (messageType.equals("error")) {
+            js.executeScript("$.growl.error({ title: 'ERROR', message: '" + message + "' });");
+        } else if (messageType.equals("info")) {
+            js.executeScript("$.growl.notice({ title: 'Notice', message: '" + message + "' });");
+        } else if (messageType.equals("warning")) {
+            js.executeScript("$.growl.warning({ title: 'Warning!', message: '" + message + "' });");
+        }
+
+        // jquery-growl w/ colorized output
 //		js.executeScript("$.growl.error({ title: 'ERROR', message: 'Some exception is coming' });");
 //		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
 //		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
-	}
-	
-	public static void takeScreenshot(String fileName) throws IOException{
-		// Take screenshot and store as a file format
-		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		// now copy the screenshot to desired location using copyFile //method
-		FileUtils.copyFile(src, 
-				new File("/Users/NaveenKhunteta/Documents/MyPOMFramework/PageObjectModel/screenshots/" + fileName +".png"));
+    }
 
-	}
+    public static void takeScreenshot(String fileName) throws IOException {
+        // Take screenshot and store as a file format
+        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // now copy the screenshot to desired location using copyFile //method
+        FileUtils.copyFile(src,
+                new File("screenshots/" + fileName + ".png"));
 
+    }
+
+    @AfterTest
+    public void endTest() {
+        driver.close();
+    }
 }
